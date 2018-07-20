a25e2870f278d276fcf76a42f6553eb07f1be5e8
Oleg <oleg.pav.m@gmail.com>
2018-07-20 11:31:08 +0300

    Skeleton.

---
+++ src/Main.js
@@ -0,0 +1,37 @@
+const core = require('griboyedov');
+const logger = core.Logger;
+const stats = core.Stats.client;
+const BasicService = core.service.Basic;
+const env = require('./Env');
+
+class Main extends BasicService {
+    constructor() {
+        super();
+
+        // TODO -
+        this.printEnvBasedConfig(env);
+        this.addNested();
+        this.stopOnExit();
+    }
+
+    async start() {
+        await this.startNested();
+        stats.increment('main_service_start');
+    }
+
+    async stop() {
+        await this.stopNested();
+        stats.increment('main_service_stop');
+        process.exit(0);
+    }
+}
+
+new Main().start().then(
+    () => {
+        logger.info('Main service started!');
+    },
+    error => {
+        logger.error(`Main service failed - ${error}`);
+        process.exit(1);
+    }
+);
---
+++ src/service/Sender.js
@@ -0,0 +1,16 @@
+const core = require('griboyedov');
+const BasicService = core.service.Basic;
+
+class Sender extends BasicService {
+    async start() {
+        // TODO -
+    }
+
+    async stop() {
+        // TODO -
+    }
+
+    async send() {
+        // TODO -
+    }
+}
